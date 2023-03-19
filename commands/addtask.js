dea20e258c94a1b281241066fab97ed9be193aa4
mng <thomassred@gmail.com>
2023-03-19 20:22:58 +0600

    taskbot init

---
+++ commands/addtask.js
@@ -0,0 +1,101 @@
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("add")
+    .setDescription("Добавить задачу.")
+    .addStringOption((option) =>
+      option
+        .setName("user")
+        .setDescription(
+          "Кому задачка ? (Необязательно, задачка может быть общая)"
+        )
+        .setRequired(false)
+    )
+    .addStringOption((option) =>
+      option
+        .setName("text")
+        .setDescription("Краткое описание задачки.")
+        .setRequired(false)
+    ),
+  async execute(interaction) {
+    if (interaction.channel.id === "1086971070004863056") {
+      // Получение аргументов
+      const user = interaction.options.get("user");
+      const text = interaction.options.get("text");
+
+      // Отправка ответа
+      interaction
+        .reply({
+          content: "Красава",
+          ephemeral: true,
+        })
+        .then(() => {
+          const successResponse = new EmbedBuilder()
+            .setColor("#141414")
+            .setTitle("Задачка создана")
+            .setDescription(
+              user
+                ? `Задачка для ${user.value} успешно создана.\n\n**${text.value}**\n\n`
+                : `Общая задачка успешно создана.\n\n**${text.value}**\n\n`
+            )
+            .setTimestamp()
+            .setFooter({
+              text: interaction.user.username,
+              iconURL: interaction.user.avatarURL(),
+            });
+          interaction.channel.send({ embeds: [successResponse] });
+
+          // Создание таска в форуме
+          const globalTasksChannel = interaction.guild.channels.cache.get(
+            "1086972110523605022"
+          );
+          const meaningTasksChannel = interaction.guild.channels.cache.get(
+            "1086971454437998633"
+          );
+          const beraTasksChannel = interaction.guild.channels.cache.get(
+            "1087005148188000319"
+          );
+          const kostochkaTasksChannel = interaction.guild.channels.cache.get(
+            "1087005105720659998"
+          );
+          console.log(user.value);
+
+          function createTaskThread(taskChannel) {
+            taskChannel.threads.create({
+              name: text.value,
+              message: {
+                content: `${
+                  user
+                    ? `${user.value} для тебя задачка.\n\n`
+                    : "Общая задачка\n\n"
+                } Подробное описание задачи и дополнительные ресурсы:`,
+              },
+              appliedTags: ["1087010624980123708"],
+            });
+          }
+
+          if (!user) {
+            createTaskThread(globalTasksChannel);
+          }
+          if (user) {
+            if (user.value === "<@247395057924177920>") {
+              createTaskThread(meaningTasksChannel);
+            }
+            if (user.value === "<@480775560620933121>") {
+              createTaskThread(beraTasksChannel);
+            }
+            if (user.value === "<@270556086308700160>") {
+              createTaskThread(kostochkaTasksChannel);
+            }
+            createTaskThread(globalTasksChannel);
+          }
+        });
+    } else {
+      interaction.reply({
+        content: "Создание задач доступно только в <#1086971070004863056>",
+        ephemeral: true,
+      });
+    }
+  },
+};
---
+++ index.js
@@ -0,0 +1,66 @@
+const {
+  Client,
+  Events,
+  GatewayIntentBits,
+  Collection,
+  EmbedBuilder,
+} = require("discord.js");
+require("dotenv").config();
+const fs = require("node:fs");
+const path = require("node:path");
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMembers,
+  ],
+});
+
+// Инициализация команд и бота
+client.once(Events.ClientReady, (client) => {
+  console.log(`[TASKBOT]Ready! Logged in as ${client.user.tag}`);
+
+  client.commands = new Collection();
+
+  const commandsPath = path.join(__dirname, "commands");
+  const commandFiles = fs
+    .readdirSync(commandsPath)
+    .filter((file) => file.endsWith(".js"));
+
+  for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+    if ("data" in command && "execute" in command) {
+      client.commands.set(command.data.name, command);
+    } else {
+      console.log(
+        `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
+      );
+    }
+  }
+});
+
+// Обработка комманд
+client.on(Events.InteractionCreate, async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+
+  const command = interaction.client.commands.get(interaction.commandName);
+
+  if (!command) {
+    console.error(`No command matching ${interaction.commandName} was found.`);
+    return;
+  }
+
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    await interaction.reply({
+      content: "There was an error while executing this command!",
+      ephemeral: true,
+    });
+  }
+});
+
+client.login(process.env.TOKEN);
