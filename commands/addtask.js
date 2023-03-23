65e1bea53f658ade33def3704d07482973e51a82
mng <thomassred@gmail.com>
2023-03-23 22:26:26 +0600

    another fix

--- commands/addtask.js
+++ commands/addtask.js
@@ -45,6 +45,36 @@ module.exports = {
               iconURL: interaction.user.avatarURL(),
             });
           interaction.channel.send({ embeds: [successResponse] });
+          // Определение пользователей к форумам
+          const channels = {
+            "1086972110523605022": interaction.guild.channels.cache.get(
+              "1086972110523605022"
+            ),
+            "1086971454437998633": interaction.guild.channels.cache.get(
+              "1086971454437998633"
+            ),
+            "1087005148188000319": interaction.guild.channels.cache.get(
+              "1087005148188000319"
+            ),
+            "1087005105720659998": interaction.guild.channels.cache.get(
+              "1087005105720659998"
+            ),
+            "1088391112420499456": interaction.guild.channels.cache.get(
+              "1088391112420499456"
+            ),
+            "<@247395057924177920>": interaction.guild.channels.cache.get(
+              "1086971454437998633"
+            ),
+            "<@480775560620933121>": interaction.guild.channels.cache.get(
+              "1087005148188000319"
+            ),
+            "<@270556086308700160>": interaction.guild.channels.cache.get(
+              "1087005105720659998"
+            ),
+            "<@473800235445911553>": interaction.guild.channels.cache.get(
+              "1088391112420499456"
+            ),
+          };
 
           //Создание треда в форуме
           const channels = require("../channels");
